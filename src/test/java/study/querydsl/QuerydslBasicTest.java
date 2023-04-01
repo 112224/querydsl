5d055577e201da4bb2470c6e4ffd4968c8662d0a
112224 <didgns25@naver.com>
2023-04-01 15:58:56 +0900

    Test: sql function 호출하기
    사용하고 있는 Dialect 에 등록된 Function을 직접 호출하여 사용할 수 있음.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -813,4 +813,42 @@ public class QuerydslBasicTest {
             System.out.println("member1 = " + member1);
         }
     }
+
+    @Test
+    public void sqlFunction() throws Exception {
+
+        //when
+        List<String> result = queryFactory
+                .select(
+                        Expressions.stringTemplate(
+                                "function('replace', {0}, {1}, {2})",
+                                member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+
+        //then
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() throws Exception {
+
+        //when
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})", member.username)
+//                ))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        //then
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
